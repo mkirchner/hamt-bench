d5dd659f6e4d2be10b849e3ce9dee1d32718f4c2
Marc Kirchner <mail@marc-kirchner.de>
2023-12-26 08:15:49 +0400

    Much nicer plots

--- bench.sh
+++ bench.sh
@@ -2,15 +2,15 @@
 
 GITCOMMIT=`(cd ../hamt && git describe --always)`
 echo "hamt"
-build/bench-hamt | sed -u -e "s/^/"libhamt",$GITCOMMIT,/" > db/import.$$
-echo "glib-hashtable"
-build/bench-glib | sed -u -e "s/^/"glib2","",/" >> db/import.$$
-echo "avl"
-build/bench-avl | sed -u -e "s/^/"avl","",/" >> db/import.$$
-echo "rb"
-build/bench-rb | sed -u -e "s/^/"rb","",/" >> db/import.$$
-echo "hsearch"
-build/bench-hsearch | sed -u -e "s/^/"hsearch","",/" >> db/import.$$
+build/bench-hamt | sed -u -e "s/^/"libhamt-pool",$GITCOMMIT,/" > db/import.$$
+# echo "glib-hashtable"
+# build/bench-glib | sed -u -e "s/^/"glib2","",/" >> db/import.$$
+# echo "avl"
+# build/bench-avl | sed -u -e "s/^/"avl","",/" >> db/import.$$
+# echo "rb"
+# build/bench-rb | sed -u -e "s/^/"rb","",/" >> db/import.$$
+# echo "hsearch"
+# build/bench-hsearch | sed -u -e "s/^/"hsearch","",/" >> db/import.$$
 
 {
 cat << EOF
